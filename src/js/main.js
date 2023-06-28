3c3175b95bfce85f36d73f34ab135fb15004c670
fernando12mais <fernandogabriel12mais@gmail.com>
2023-06-28 13:05:53 -0300

    Feat: Fix js errors and adds load more functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,54 @@
+var data = [];
+
 var reportsWidget = {
-    options: {
-        containerSelector: '.reports',
-        template: (
-            '{{#.}}' +
-                '<article class="reports_item">' +
-                    '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
-                    '</a>' +
-                    '<footer class="reports_docs">' +
-                        '{{#documents}}' +
-                            '<h3 class="reports_title">' +
-                                '<a href="{{url}}" target="_blank">{{title}}</a>' +
-                            '</h3>' +
-                        '{{/documents}}' +
-                    '</footer>' +
-                '</article>' +
-            '{{/.}}'
-        )
-    },
+  options: {
+    containerSelector: ".reports",
+    template: `{{#.}}
+                <article>
+                    <a href="{{cover}}" target="_blank">
+                        <img  src="{{cover}}" alt="{{title}} Cover"/>
+                    </a>
+                    <footer >
+                        {{#documents}}
+                            <h3>
+                                <a href="{{url}}" target="_blank">{{title}} 
+                                <span>({{file_size}} {{file_type}})</span>
+                                
+                                </a>
+                      
+                            </h3>
+                        {{/documents}} 
+                    </footer>
+                </article> 
+            {{/.}}`,
+  },
 
-    init: function() {
-        this.renderReports(reportData || []);
-    },
+  init: function () {
+    data = reportData.slice(0, 6);
+    this.renderReports(data);
+  },
 
-    renderReports: function(reports) {
-        var inst = this,
-            options = inst.options;
+  renderReports: function (reports) {
+    var inst = this,
+      options = inst.options;
 
-        $(options.containerSelector).html(Mustache.render(options.template, reports));
-    }
+    $(options.containerSelector).html(
+      Mustache.render(options.template, reports)
+    );
+  },
 };
 
 reportsWidget.init();
+
+const button = document.querySelector("#btn-load-reports");
+
+button.addEventListener("click", function () {
+  data = data.concat(reportData.slice(data.length - 1, 6));
+
+  console.log(data);
+  reportsWidget.renderReports(data);
+
+  if (data.length == reportData.length) {
+    button.style.display = "none";
+  }
+});
